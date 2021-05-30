40e6886b15150e643001e8b2939c0554959fbf8c
Hau Nguyen <hauvipapro@gmail.com>
2021-05-30 15:36:41 +0000

    refactor: move template error to const

--- color.go
+++ color.go
@@ -6,9 +6,15 @@ import (
 	"github.com/fatih/color"
 )
 
+const templateErr = "[%s error]: "
+
 var fmtErr = color.New(color.FgRed)
 
 func PrintAppError(app string, msg string) {
-	fmtErr.Printf("[%s error]: ", app)
+	printAppColor(app, msg, templateErr, fmtErr)
+}
+
+func printAppColor(app, msg, template string, c *color.Color) {
+	c.Printf(template, app)
 	fmt.Println(msg)
 }
