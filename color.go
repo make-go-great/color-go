c391d331828defe3cdef836f4bf709cc83e898f9
Hau Nguyen <hauvipapro@gmail.com>
2022-07-22 10:38:32 +0700

    feat: add PrintAppWarning/OK

--- color.go
+++ color.go
@@ -8,10 +8,22 @@ import (
 
 const templateErr = "[%s error]: "
 
-var fmtErr = color.New(color.FgRed)
+var (
+	fmtErr     = color.New(color.FgRed)
+	fmtWarning = color.New(color.FgYellow)
+	fmtOK      = color.New(color.FgGreen)
+)
 
 func PrintAppError(app string, msg string) {
 	printAppColor(app, msg, templateErr, fmtErr)
+}
+
+func PrintAppWarning(app string, msg string) {
+	printAppColor(app, msg, templateErr, fmtWarning)
+}
+
+func PrintAppOK(app string, msg string) {
+	printAppColor(app, msg, templateErr, fmtOK)
 }
 
 func printAppColor(app, msg, template string, c *color.Color) {
